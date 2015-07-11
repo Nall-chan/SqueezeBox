4e1316860e825eb92d00fc0dcf6d81e6b8bac1c6
Nall-chan <mt@neo-ami.de>
2015-07-11 11:45:35 +0200

    no message

--- SqueezeBoxDevice/module.php
+++ SqueezeBoxDevice/module.php
@@ -109,7 +109,38 @@ public function RequestState()
     protected function SetSummary($data) {
         IPS_LogMessage(__CLASS__, __FUNCTION__ . "Data:" . $data); //                   
     }
-
+		//Remove on next Symcon update
+		protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize) {
+		
+			if(!IPS_VariableProfileExists($Name)) {
+				IPS_CreateVariableProfile($Name, 1);
+			} else {
+				$profile = IPS_GetVariableProfile($Name);
+				if($profile['ProfileType'] != 1)
+					throw new Exception("Variable profile type does not match for profile ".$Name);
+			}
+			
+			IPS_SetVariableProfileIcon($Name, $Icon);
+			IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+			IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
+			
+		}		
+		protected function RegisterProfileIntegerEx($Name, $Icon, $Prefix, $Suffix, $Associations) {
+                        if ( sizeof($Associations) === 0 ){
+                          $MinValue = 0;
+                          $MaxValue = 0;
+                        } else {
+                          $MinValue = $Associations[0][0];
+                          $MaxValue = $Associations[sizeof($Associations)-1][0];
+                        }
+		
+			$this->RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, 0);
+		
+			foreach($Associations as $Association) {
+				IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
+			}
+			
+		}
 }
 
 ?>
