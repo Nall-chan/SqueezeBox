a258e797332b259691706b9ddbce77697d8f91cc
Nall-chan <mt@neo-ami.de>
2015-07-11 22:15:35 +0200

    no message

--- SqueezeBoxDevice/module.php
+++ SqueezeBoxDevice/module.php
@@ -13,6 +13,7 @@ class SqueezeboxDevice extends IPSModule
         //These lines are parsed on Symcon Startup or Instance creation
         //You cannot use variables here. Just static values.
         $this->RegisterPropertyString("MACAddress", "");
+        $this->RegisterPropertyBoolean("EmulateStatus", false);
     }
 
     public function ApplyChanges()
@@ -38,21 +39,26 @@ class SqueezeboxDevice extends IPSModule
             Array(2, "Album", "", -1)
         ));
         $this->RegisterProfileInteger("Volume.Squeezebox", "Intensity", "", " %", 0, 100, 1);
-        $this->RegisterVariableBoolean("Power", "Power", "~Switch");
+
+        $this->RegisterVariableBoolean("Power", "Power", "~Switch", 1);
         $this->EnableAction("Power");
-        $this->RegisterVariableInteger("Status", "Status", "Status.Squeezebox");
+        $this->RegisterVariableInteger("Status", "Status", "Status.Squeezebox", 2);
         $this->EnableAction("Status");
-        $this->RegisterVariableInteger("Volume", "Volume", "Volume.Squeezebox");
+        $this->RegisterVariableInteger("Volume", "Volume", "Volume.Squeezebox", 3);
         $this->EnableAction("Volume");
-        $this->RegisterVariableInteger("Shuffle", "Shuffle", "Shuffle.Squeezebox");
+        $this->RegisterVariableInteger("Shuffle", "Shuffle", "Shuffle.Squeezebox", 4);
         $this->EnableAction("Shuffle");
-        $this->RegisterVariableInteger("Repeat", "Shuffle", "Repeat.Squeezebox");
+        $this->RegisterVariableInteger("Repeat", "Repeat", "Repeat.Squeezebox", 5);
         $this->EnableAction("Repeat");
+
+        $this->RegisterVariableString("Interpret", "Interpret", "", 6);
+        $this->RegisterVariableString("Title", "Title", "", 7);
+        $this->RegisterVariableString("Album", "Album", "", 8);
+        $this->RegisterVariableString("Cover", "Cover", "", 9);
+        $this->RegisterVariableString("Position", "Position", "", 10);
+        $this->RegisterVariableString("Duration", "Dauer", "", 11);
+
         $this->MAC = $this->GetMAC($this->ReadPropertyString('MACAddress'));
-        if ($this->MAC === false)
-        {
-            IPS_LogMessage("IODevice ApplyChanges", "Invalid MAC".$this->ReadPropertyString('MACAddress'));                        
-        }
     }
 
 ################## PRIVATE     
@@ -61,9 +67,176 @@ class SqueezeboxDevice extends IPSModule
     {
         return IPS_SendDataToParent($this->InstanceID, json_encode(Array("DataID" => "{EDDCCB34-E194-434D-93AD-FFDF1B56EF38}", "Buffer" => $Text)));
     }
+
     private function GetMAC($mac)
     {
-        return $this->MAC = strtoupper(str_replace(array("-",":"), "", $mac)); 
+        return $this->MAC = strtoupper(str_replace(array("-", ":"), "", $mac));
+    }
+
+    private function Cover($var_id)
+// setzt die $var_id mit dem Coverbild von $player_id
+    {
+        $ParentID = $this->GetParent();
+        if (!($ParentID === false))
+        {
+            $SQserverIP = IPS_GetProperty($ParentID, 'Host');
+            $time = time();
+            $player_id = urlencode(implode(":", str_split($this->MAC, 2)));
+            $str = "<table width='100%' cellspacing='0'><tr><td align=right>";
+            $str = $str . "<img src='http://" . $SQserverIP . "/music/current/cover_150x150_$time.jpg?player=$player_id'></img>";
+            $str = $str . "</td></tr></table>";
+        }
+        else
+            $str = "";
+        SetValueString($var_id, $str);
+    }
+
+    private function decode($array)
+    {
+        $powerID = $this->GetIDForIdent("Power");
+        $volumeID = $this->GetIDForIdent("Volume");
+        $modusID = $this->GetIDForIdent("Status");
+        $repeatID = $this->GetIDForIdent("Repeat");
+        $shuffleID = $this->GetIDForIdent("Shuffle");
+        $coverID = $this->GetIDForIdent("Cover");
+        $albumID = $this->GetIDForIdent("Album");
+        $interpretID = $this->GetIDForIdent("Interpret");
+        $durationID = $this->GetIDForIdent("Duration");
+        $positionID = $this->GetIDForIdent("Position");
+        $titleID = $this->GetIDForIdent("Title");
+
+        if ($array[1] == 'power')
+        {
+            if ($array[2] == '1')
+            {
+                SetValueBoolean($powerID, true);
+            }
+            else
+            {
+                SetValueBoolean($powerID, false);
+            }
+        }
+        //Lautstärke bei Änderung aktualisieren
+        if (($array[1] == 'prefset') and ( $array[3] == 'volume'))
+        {
+            SetValue($volumeID, urldecode($array[4]));
+        }
+        // Repeat bei Änderung aktualisieren
+        if (($array[1] == 'prefset') and ( $array[3] == 'repeat'))
+        {
+            SetValue($repeatID, $array[4]);
+        }
+        // Shuffle bei Änderung aktualisieren
+        if (($array[1] == 'prefset') and ( $array[3] == 'shuffle'))
+            SetValue($shuffleID, $array[4]);
+
+        //Titel-Tag aktualisieren
+        if (($array[1] == 'playlist') and ( $array[2] == 'newsong'))
+        {
+            SetValue($titleID, utf8_decode(urldecode($array[3])));
+            SetValue($modusID, 2); // Button auf play
+            // Subscripe auf entsprechende Box für Anzeige der Laufzeit
+            /*            CSCK_SendText(SqueezeServer, $array[0] . " status - 1 subscribe:2" . chr(13));
+              SqueezeSend($array[0], "artist ?");
+              SqueezeSend($array[0], "album ?"); */
+            IPS_Sleep(10);
+
+            $this->Cover($coverID); // Cover anzeigen
+        }
+        // Album aktualisieren
+        if ($array[1] == 'album')
+        {
+            if (isset($array[2]))
+                SetValue($albumID, utf8_decode(urldecode($array[2])));
+            else
+                SetValue($albumID, '');
+        }
+        // Artist aktualisieren
+        if ($array[1] == 'artist')
+        {
+            if (isset($array[2]))
+                SetValue($interpretID, utf8_decode(urldecode($array[2])));
+            else
+                SetValue($interpretID, '');
+        }
+        // Steuerungstasten im Webfront aktualisieren
+        if ($array[1] == 'play')
+        {
+            SetValue($modusID, 2);
+        }
+        if ($array[1] == 'stop')
+        {
+            SetValue($modusID, 0);
+        }
+        if (($array[1] == 'pause') and ( $array[2] == 1))
+        {
+            SetValue($modusID, 1);
+        }
+        if (($array[1] == 'pause') and ( $array[2] == 0))
+        {
+            SetValue($modusID, 2);
+        }
+        if (($array[1] == 'button') and ( $array[2] == 'jump_rew'))
+        {
+            SetValue($modusID, 4);
+        }
+        if (($array[1] == 'button') and ( $array[2] == 'jump_fwd'))
+        {
+            SetValue($modusID, 3);
+        }
+        if (($array[1] == 'status') and ( $array[4] == 'subscribe%3A2'))
+        {
+            foreach ($array as $item)
+            {
+                $item = utf8_decode(urldecode($item));
+                $chunks = explode(":", $item);
+                if ($chunks[0] == "time")
+                {
+                    SetValueString($positionID, @date('i:s', $chunks[1]));
+//                IPS_LogMessage("Squeeze",date('i:s', $chunks[1]));
+                }
+                if ($chunks[0] == "duration")
+                {
+                    SetValueString($durationID, @date('i:s', $chunks[1]));
+//                IPS_LogMessage("Squeeze",date('i:s', $chunks[1]));
+                }
+                if ($chunks[0] == "mode")
+                {
+                    if ($chunks[1] == 'play')
+                    {
+                        SetValue($modusID, 2);
+                    }
+                    if ($chunks[1] == 'stop')
+                    {
+                        SetValue($modusID, 0);
+                    }
+                    if ($chunks[1] == 'pause')
+                    {
+                        SetValue($modusID, 1);
+                    }
+                }
+//signalstrength%3A88 rate%3A1 can_seek%3A1  playlist%20mode%3Aoff seq_no%3A91 playlist_cur_index%3A39 playlist_timestamp%3A1415459866.17632 playlist_tracks%3A42 playlist%20index%3A39 id%3A26977 title%3AMonster%20High%20-%20Flederm%C3%A4use%20im%20Bauch%20-%20H%C3%B6rbuch%20-%20Abschnitt%2040 genre%3AH%C3%B6rbuch artist%3AMonster%20High album%3AMonster%20High%20-%20Flederm%C3%A4use%20im%20Bauch%20-%20H%C3%B6rbuch duration%3A614.034
+                if ($chunks[0] == 'mixer volume')
+                {
+                    SetValue($volumeID, urldecode($chunks[1]));
+                }
+                if ($chunks[0] == 'playlist repeat')
+                    SetValue($repeatID, $chunks[1]);
+                if ($chunks[0] == 'playlist shuffle')
+                    SetValue($shuffleID, $chunks[1]);
+                if ($chunks[0] == 'power')
+                {
+                    if ($chunks[1] == '1')
+                    {
+                        SetValueBoolean($powerID, true);
+                    }
+                    else
+                    {
+                        SetValueBoolean($powerID, false);
+                    }
+                }
+            }
+        }
     }
 
 ################## PUBLIC
@@ -88,14 +261,17 @@ class SqueezeboxDevice extends IPSModule
         // CB5950B3-593C-4126-9F0F-8655A3944419 ankommend von Splitter
         $data = json_decode($JSONString);
         $this->MAC = $this->GetMAC($this->ReadPropertyString('MACAddress'));
-        if ($this->MAC === false) return false;
+        if ($this->MAC === false)
+            return false;
         IPS_LogMessage("IODevice MAC", $data->MAC);
-        IPS_LogMessage("IODevice MAC", $this->MAC);        
+        IPS_LogMessage("IODevice MAC", $this->MAC);
         if ($this->MAC == $data->MAC)
         {
-            IPS_LogMessage("IODevice DATA", print_r($data->Payload, 1));            
+            IPS_LogMessage("IODevice DATA", print_r($data->Payload, 1));
+            $this->decode((array) $data->Payload);
             return true;
-        } else
+        }
+        else
             return false;
         //We would parse our payload here before sending it further...
     }
