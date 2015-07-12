98767efa56f6d8e6b478a7b0bc424d92b5b7d505
Nall-chan <mt@neo-ami.de>
2015-07-12 18:14:48 +0200

    no message

--- SqueezeBoxDevice/module.php
+++ SqueezeBoxDevice/module.php
@@ -61,7 +61,7 @@ class SqueezeboxDevice extends IPSModule
 
 
         $this->RegisterVariableInteger("Index", "Playlist Position", "", 12);
-        $this->RegisterVariableInteger("Signal", utf8_encode("Signalstärke"), "~Intensity", 13);
+        $this->RegisterVariableInteger("Signal", utf8_encode("Signalstärke"), "~Intensity.100", 13);
         $this->RegisterVariableInteger("Tracks", "Playlist Anzahl Tracks", "", 14);
         $this->RegisterVariableString("Genre", "Stilrichtung", "", 15);
 
