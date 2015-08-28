e6d29bfb0132afabc452e0bf2b310eb8646b410d
Nall-chan <mt@neo-ami.de>
2015-08-28 09:42:57 +0200

    no message

--- SqueezeBoxDevice/module.php
+++ SqueezeBoxDevice/module.php
@@ -18,7 +18,7 @@ class SqueezeboxDevice extends IPSModule
         // 1. Verfügbarer LMS-Splitter wird verbunden oder neu erzeugt, wenn nicht vorhanden.
         $this->ConnectParent("{96A9AB3A-2538-42C5-A130-FC34205A706A}");
         IPS_Logmessage('Setting_isOld',print_r(IPS_GetProperty($this->InstanceID, 'isOld'),1));
-        if ((bool) IPS_GetProperty($this->InstanceID, 'isOld'))
+        if ((integer)IPS_GetProperty($this->InstanceID, 'isOld') == 1)
         {
             $Address = IPS_GetProperty($this->InstanceID, 'Address');
             $Interval = IPS_GetProperty($this->InstanceID, 'Interval');
@@ -33,7 +33,7 @@ class SqueezeboxDevice extends IPSModule
         IPS_Logmessage('Setting_Address',print_r($Address,1));
         IPS_Logmessage('Setting_Interval',print_r($Interval,1));
         IPS_Logmessage('Setting_CoverSize',print_r($CoverSize,1));
-        $this->RegisterPropertyBoolean("isOld", true);
+        $this->RegisterPropertyInteger("isOld", 1);
         $this->RegisterPropertyString("Address", $Address);
         $this->RegisterPropertyInteger("Interval", $Interval);
         $this->RegisterPropertyString("CoverSize", $CoverSize);
