328108ed67b768e916c62a2d4f634806a91d8c0e
Nall-chan <mt@neo-ami.de>
2015-09-09 21:15:14 +0200

    Add Workaround for  PHP5.4

--- SqueezeBoxClass.php
+++ SqueezeBoxClass.php
@@ -485,4 +485,16 @@ class LSQResponse extends stdClass
       } */
 }
 
+if(!function_exists("array_column"))
+{
+
+    function array_column($array,$column_name)
+    {
+
+        return array_map(function($element) use($column_name){return $element[$column_name];}, $array);
+
+    }
+
+}
+
 ?>
