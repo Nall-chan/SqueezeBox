8a6a3590e6103afdb182136050349030ffca3996
Nall-chan <micha@nall-chan.net>
2019-04-24 15:26:19 +0000

    Apply fixes from StyleCI

--- SqueezeBoxAlarm/module.php
+++ SqueezeBoxAlarm/module.php
@@ -216,7 +216,6 @@ class LSA_Alarm
     {
         $this->Time = ((($Time[0] * 60) + $Time[1]) * 60) + $Time[2];
     }
-
 }
 
 /**
@@ -333,7 +332,6 @@ class LSA_AlarmList
         }
         return false;
     }
-
 }
 
 /**
@@ -352,7 +350,6 @@ class LSA_AlarmList
  */
 class SqueezeboxAlarm extends IPSModule
 {
-
     use \squeezebox\VariableProfileHelper,
         LMSHTMLTable,
         \squeezebox\DebugHelper,
@@ -649,7 +646,7 @@ class SqueezeboxAlarm extends IPSModule
      */
     protected function ProcessHookdata()
     {
-        if ((!isset($_GET['ID'])) or ( !isset($_GET['Type'])) or ( !isset($_GET['Secret']))) {
+        if ((!isset($_GET['ID'])) or (!isset($_GET['Type'])) or (!isset($_GET['Secret']))) {
             echo $this->Translate('Bad Request');
             return;
         }
@@ -1132,7 +1129,7 @@ class SqueezeboxAlarm extends IPSModule
             return false;
         }
         $LMSResponse->SliceData();
-        if ((count($LMSResponse->Data) == 0) or ( $LMSResponse->Data[0] == '?')) {
+        if ((count($LMSResponse->Data) == 0) or ($LMSResponse->Data[0] == '?')) {
             trigger_error($this->Translate('Player not connected'), E_USER_NOTICE);
             return false;
         }
@@ -1366,7 +1363,7 @@ class SqueezeboxAlarm extends IPSModule
             trigger_error(sprintf($this->Translate('%s out of range.'), 'AlarmIndex'), E_USER_NOTICE);
             return false;
         }
-        if (($Url == '0') or ( $Url == '')) {
+        if (($Url == '0') or ($Url == '')) {
             $Url = 'CURRENT_PLAYLIST';
         }
 
@@ -1393,7 +1390,7 @@ class SqueezeboxAlarm extends IPSModule
             trigger_error(sprintf($this->Translate('%s must be integer.'), 'Value'), E_USER_NOTICE);
             return false;
         }
-        if (($Value < 0) or ( $Value > 2)) {
+        if (($Value < 0) or ($Value > 2)) {
             trigger_error(sprintf($this->Translate('%s must be 0, 1 or 2.'), 'Value'), E_USER_NOTICE);
             return false;
         }
@@ -1808,7 +1805,7 @@ class SqueezeboxAlarm extends IPSModule
                 $this->RefreshEvents($this->Alarms);
                 break;
             case 'client':
-                if (($LMSData->Data[0] == 'new') or ( $LMSData->Data[0] == 'reconnect')) {
+                if (($LMSData->Data[0] == 'new') or ($LMSData->Data[0] == 'reconnect')) {
                     $this->RequestAllState();
                 }
                 break;
@@ -1945,7 +1942,6 @@ class SqueezeboxAlarm extends IPSModule
         }
         return null;
     }
-
 }
 
 /** @} */
